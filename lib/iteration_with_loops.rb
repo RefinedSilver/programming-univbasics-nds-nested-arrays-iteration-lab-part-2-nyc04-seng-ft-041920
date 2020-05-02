92f1999104cd470e4aa74baa2786fa6d4e0ff6ef
Ian McKenzie <imckenzie777@gmail.com>
2020-05-02 19:47:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,7 +13,7 @@ def find_min_in_nested_arrays(src)
       end
       element_index += 1
     end
-    p array_results << min_value
+    array_results << min_value
     row_index += 1
   end
   array_results
